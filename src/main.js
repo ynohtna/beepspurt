e50e4a055ecbe0112a866d9a9456bb625d6560a9
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2016-03-16 22:30:37 +0000

    settings.js: explicitly request plask window to be centered on primary display; import babel-polyfill. (+2 squashed commits) Squashed commits: [060d4f5] Revert "settings.js: explicitly request plask window to be centered on primary dispaly."
    This reverts commit 1aac67039379c497c20ae2d2a83747c2d81cbdc2.
    [1aac670] settings.js: explicitly request plask window to be centered on primary dispaly.

---
+++ src/main.js
@@ -0,0 +1,33 @@
+import 'babel-polyfill';
+import plask from 'plask';
+import settings from './settings';
+
+const state = {
+  i: 0,
+  msg: '[beep]'
+};
+
+plask.simpleWindow({
+  settings,
+  init: function init() { // eslint-disable-line object-shorthand
+    const { paint } = this;
+    paint.setFontFamily('Monaco', '14');
+    paint.setTextSize(128);
+
+    console.log(settings);	// eslint-disable-line no-console
+  },
+  draw: function draw() { // eslint-disable-line object-shorthand
+    const { canvas, paint } = this;
+    const b = (Math.random() * 255) | 0;
+    const i = state.i;
+
+    canvas.clear((i * 2) & 0xff, ~i, b, 0);
+
+    if ((i >> 3) & 0x01) {
+      paint.setColor(255, 255, 255, 255);
+      canvas.drawText(paint, state.msg, 200, 260);
+    }
+
+    state.i = (i + 1) & 0xff;
+  }
+});
---
+++ src/settings.js
@@ -0,0 +1,8 @@
+const settings = {
+  width: 800,
+  height: 600,
+  framerate: 30,
+  title: '[beep]spurt',
+  type: '3d2d'
+};
+export default settings;
