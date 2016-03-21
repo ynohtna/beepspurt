81f0712bec24fddff4f142d996d3a7cb85a6555a
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2016-03-21 22:21:02 +0000

    beepspurt server: basic restify server (defaulting to port 9336) and accompanying npm run scripts & altered entry point in unified rollup config for server build.

--- rollup.config.js
+++ rollup.config.js
@@ -11,9 +11,10 @@ import uglify from 'rollup-plugin-uglify';
 
 import pack from './package.json';
 
+const serverBuild = (process.argv[2] === 'server');
 const devBuild = (process.env && (process.env.NODE_ENV === 'dev')) ||
                  (process.argv[2] === 'dev');
-process.env.NODE_ENV = devBuild ? 'dev' : 'prod';
+process.env.NODE_ENV = serverBuild ? 'server' : (devBuild ? 'dev' : 'prod');
 
 const banner =`/*!
  * ${pack.name} v${pack.version}
@@ -21,13 +22,13 @@ const banner =`/*!
  * Released under the ${pack.license} License.
  */`;
 
-const entryFile = pack['jsnext:main'] || 'src/index.js';
+const entryFile = serverBuild ? 'server.js' : (pack['jsnext:main'] || 'src/index.js');
 const entry = p.resolve(entryFile);
 
-const bundleName = pack.name + (devBuild ? '.js' : '.min.js');
+const bundleName = pack.name + (serverBuild ? '.server.js' : (devBuild ? '.js' : '.min.js'));
 const dest = p.resolve(`dist/${bundleName}`);
 
-const external = ['plask'];
+const external = ['dgram', 'events', 'plask'];
 
 const babelConfig = JSON.parse(fs.readFileSync('.babelrc', 'utf-8'));
 babelConfig.babelrc = false;
@@ -35,18 +36,14 @@ babelConfig.presets = babelConfig.presets.map((preset) =>
   preset === 'es2015' ? 'es2015-rollup' : preset
 );
 
+const resolveConfig = {
+  jsnext: true,
+  main: true,
+  preferBuiltins: true
+};
+
 const plugins = [
-  nodeResolve({
-    jsnext: true,
-    main: true,
-    preferBuiltins: true,
-    external: [
-      'dgram',
-      'events'
-    ]
-  }),
-  eslint(),
-  babel(babelConfig)
+  nodeResolve(resolveConfig), eslint(), babel(babelConfig)
 ];
 if (!devBuild) {
   const uglifyConfig = JSON.parse(fs.readFileSync('.uglifyrc.json', 'utf-8'));
