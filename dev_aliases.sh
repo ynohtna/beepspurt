899a6dc788b65951d42685ae917036aeca751344
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2016-03-31 18:26:53 +0100

    Dev env: added node-notifier to npm packages; dev aliases post an OSX notification upon completion.

--- dev_aliases.sh
+++ dev_aliases.sh
@@ -1,3 +1,3 @@
-alias b="npm run build:server:dev"
-alias c="npm run build:client:dev"
+alias b="npm run build:server:dev; and notify -t 'build:SERVER:dev' -m 'OKAY'"
+alias c="npm run build:client:dev; and notify -t 'build:CLIENT:dev' -m 'OKAY'"
 alias s="npm run start:dev"
