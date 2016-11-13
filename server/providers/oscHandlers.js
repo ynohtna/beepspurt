b59b6588e2d33b6331948928b9bbd24e46bb6339
Anthony Bowyer-Lowe <ynohtna@ynohtna.org>
2016-11-13 17:58:46 +0000

    Server: CSS colour parsing supports OSC array messages of form ffff/iiii RGB(A), and parsing of clear colour inputs.

--- server/providers/oscHandlers.js
+++ server/providers/oscHandlers.js
@@ -5,7 +5,7 @@ import parseCSSColor from './csscolorparser';
 
 const parseColourMsg = name => msg => {
   const action = { type: `/renderer/${name}`, payload: [0, 0, 0, 0] };
-  const input = msg.args[0];
+  const input = msg.args.length === 1 ? msg.args[0] : msg.args;
   console.log(`${name} input: ${input}`);
   const colour = parseCSSColor(input);
   if (colour) {
@@ -18,7 +18,8 @@ const parseColourMsg = name => msg => {
 // TODO: Whitelist accepted OSC messages. Coerce/validate args.
 const oscHandlers = {
   '/renderer/FOREGROUND': parseColourMsg('FOREGROUND'),
-  '/renderer/BACKGROUND': parseColourMsg('BACKGROUND')
+  '/renderer/BACKGROUND': parseColourMsg('BACKGROUND'),
+  '/renderer/CLEAR_COLOUR': parseColourMsg('CLEAR_COLOUR')
 };
 
 export default oscHandlers;
